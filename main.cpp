ff27eb550e727d2ca5b81c02ae6a7ccd15eec42d
Oleksandr Bazhyn <olexandrbazhyn@gmail.com>
2023-10-23 21:59:50 +0300

    added mutex

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <execution>
 #include <thread>
 #include <future>
+#include <mutex>
 
 
 std::vector<int> generateRandomVector(int size)
@@ -73,19 +74,23 @@ void printMinTime(std::vector<double> times)
 		}
 	}
 
-	std::cout << "The smallest number: " << minTime << " ms" << std::endl;
+	std::cout << "The smallest number: " << minTime << " ms" << std::endl << std::endl;
 }
 
-std::vector<std::vector<int>> splitArray(const std::vector<int>& inputArray, int K) {
+std::vector<std::vector<int>> splitArray(const std::vector<int>& inputArray, int K)
+{
     std::vector<std::vector<int>> result;
     int arraySize = inputArray.size();
     int subArraySize = arraySize / K;
     int extraElements = arraySize % K;
 
     int currentIndex = 0;
-    for (int i = 0; i < K; i++) {
+    for (int i = 0; i < K; i++)
+    {
         int subArrayLength = subArraySize + (extraElements-- > 0 ? 1 : 0);
-        if (subArrayLength > 0) {
+
+        if (subArrayLength > 0)
+        {
             result.push_back(std::vector<int>(inputArray.begin() + currentIndex, inputArray.begin() + currentIndex + subArrayLength));
             currentIndex += subArrayLength;
         }
@@ -94,18 +99,22 @@ std::vector<std::vector<int>> splitArray(const std::vector<int>& inputArray, int
     return result;
 }
 
-int ScalarProduction(const std::vector<int>& A, const std::vector<int>& B) {
+int ScalarProduction(const std::vector<int>& A, const std::vector<int>& B)
+{
     int result = 0;
-    for (int i = 0; i < A.size(); i++) {
+    for (int i = 0; i < A.size(); i++)
+    {
         result += A[i] * B[i];
     }
     return result;
 }
 
-double myAlgorithm(int K, std::vector<int>& A, std::vector<int>& B) {
+double myAlgorithm(int K, std::vector<int>& A, std::vector<int>& B)
+{
     auto start = std::chrono::high_resolution_clock::now();
 
-    if (A.size() != B.size()) {
+    if (A.size() != B.size())
+    {
         throw("Error: vectors have different sizes");
     }
 
@@ -114,22 +123,29 @@ double myAlgorithm(int K, std::vector<int>& A, std::vector<int>& B) {
     std::vector<std::vector<int>> subArraysB = splitArray(B, K);
 
     int totalResult = 0;
+    std::mutex resultMutex;
 
-    for (int i = 0; i < K; i++) {
+    for (int i = 0; i < K; i++)
+    {
         std::vector<std::thread> threads;
-        int result = 0;
 
-        for (int j = 0; j < num_threads && i < K; j++, i++) {
-            threads.emplace_back([i, &result, &subArraysA, &subArraysB]() {
-                result = ScalarProduction(subArraysA[i], subArraysB[i]);
+        for (int j = 0; j < num_threads && i < K; j++, i++)
+        {
+            threads.emplace_back(
+                [i, &resultMutex, &totalResult, &subArraysA, &subArraysB]()
+                {
+                    int result = ScalarProduction(subArraysA[i], subArraysB[i]);
+
+                    std::lock_guard<std::mutex> lock(resultMutex);
+
+                    totalResult += result;
                 });
         }
 
-        for (auto& thread : threads) {
+        for (auto& thread : threads)
+        {
             thread.join();
         }
-
-        totalResult += result;
     }
 
     auto stop = std::chrono::high_resolution_clock::now();
@@ -144,7 +160,8 @@ double myAlgorithm(int K, std::vector<int>& A, std::vector<int>& B) {
 
 int main()
 {
-    std::vector<int> arr = generateRandomVector(300000000);
+    //std::vector<int> arr = generateRandomVector(300000000);
+    std::vector<int> arr = generateRandomVector(3000); //DEBUG
 
     std::cout << "EXECUTION POLICY: none" << std::endl;
     double time1 = measureTransformReduceTime(arr.begin(), arr.end(), 0, std::plus<>(), [](int i) { return i * i; });
@@ -161,11 +178,11 @@ int main()
     std::cout << "EXECUTION POLICY: unsequenced policy" << std::endl;
     double time5 = measureTransformReduceTime(std::execution::unseq, arr.begin(), arr.end(), 0, std::plus<>(), [](int i) { return i * i; });
 
-    double time6 = myAlgorithm(3, arr, arr);
-
-
-    std::vector<double> times = { time1, time2, time3, time4, time5, time6 };
+    std::vector<double> times = { time1, time2, time3, time4, time5 };
     printMinTime(times);
+
+    std::cout << "My Algorithm" << std::endl;
+    double time6 = myAlgorithm(3, arr, arr);    
 
     return 0;
 }
